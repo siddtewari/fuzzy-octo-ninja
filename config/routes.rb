5b29e59e901fefeadc215e693d4994efddb36ee9
siddtewari <sidtewari@gmail.com>
2013-04-30 07:05:32 -0400

    Dashboard page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Zhouster::Application.routes.draw do
-  
+	constraints Clearance::Constraints::SignedIn.new do
+    root to: 'dashboards#show'
+  end
+
+	root to: "homes#show"
+
+	resource :dashboard, only: [:show]  
 end
